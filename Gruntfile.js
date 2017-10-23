1f1ea4950e698c71a41ff68487b45aea4ecf6849
Anton Fletcher <salmonmoose@gmail.com>
2017-10-23 16:48:48 +1030

    custom fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,18 @@ module.exports = function (grunt) {
 
   var game = {
     modifiers: grunt.file.readJSON('data/modifiers.json'),
+    races: {
+      "Dwarf": grunt.file.readJSON('data/dwarf.json')
+    },
     classes: {
       "Cleric": grunt.file.readJSON('data/cleric.json')
     },
     spells: {
       "cantrips": grunt.file.readJSON('data/cantrips.json'),
-      "level_1": grunt.file.readJSON('data/level_1.json')
-    }
+      "level_1": grunt.file.readJSON('data/level_1.json'),
+      "level_2": grunt.file.readJSON('data/level_2.json')
+    },
+    proficiencies: grunt.file.readJSON('data/proficiencies.json')
   };
 
   grunt.initConfig({
@@ -28,18 +33,40 @@ module.exports = function (grunt) {
             'metalsmith-pug': {
               pretty: true,
               locals: {
-                title: 'Adrik Ungart',
                 character: grunt.file.readJSON('adrik.json'),
                 game: game
               }
+            },
+
+            'metalsmith-assets-improved': {
+              src: 'node_modules/components-font-awesome/fonts',
+              dest: "fonts"
             }
           }
         }
+      }
+    },
+
+    copy: {
+      fonts: {
+        files: [
+          {
+            expand: true,
+            src: [
+              'node_modules/components-font-awesome/fonts/*',
+              'node_modules/rpg-awesome/fonts/*'
+            ],
+            dest: 'contents/fonts/',
+            filter: 'isFile',
+            flatten: true
+          }
+        ]
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-metalsmith');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.registerTask('default', [
     'metalsmith'
