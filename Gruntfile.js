d3d6b89a775614abd75b29d2748a8439e461b33d
Anton Fletcher <salmonmoose@gmail.com>
2017-10-23 13:43:36 +1030

    add some spells

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,14 @@
 module.exports = function (grunt) {
 
-  var modifiers = {
-    1: -5,
-    2: -4,
-    3: -4,
-    4: -3,
-    5: -3,
-    6: -2,
-    7: -2,
-    8: -1,
-    9: -1,
-    10: 0,
-    11: 0,
-    12: 1,
-    13: 1,
-    14: 2,
-    15: 2,
-    16: 3,
-    17: 3,
-    18: 4,
-    19: 4,
-    20: 5,
-    21: 5,
-    22: 6,
-    23: 6,
-    24: 7,
-    25: 7,
-    26: 8,
-    27: 8,
-    28: 9,
-    29: 9,
-    30: 10
+  var game = {
+    modifiers: grunt.file.readJSON('data/modifiers.json'),
+    classes: {
+      "Cleric": grunt.file.readJSON('data/cleric.json')
+    },
+    spells: {
+      "cantrips": grunt.file.readJSON('data/cantrips.json'),
+      "level_1": grunt.file.readJSON('data/level_1.json')
+    }
   };
 
   grunt.initConfig({
@@ -52,7 +30,7 @@ module.exports = function (grunt) {
               locals: {
                 title: 'Adrik Ungart',
                 character: grunt.file.readJSON('adrik.json'),
-                modifiers: modifiers
+                game: game
               }
             }
           }
